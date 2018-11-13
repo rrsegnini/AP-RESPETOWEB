3d923affbbd56a6469c9e9359d55f22f9f158f90
José Luis <joserh1206@gmail.com>
2018-11-13 16:00:41 -0600

    Denuncia

--- js/denuncias.js
+++ js/denuncias.js
@@ -1,18 +1,126 @@
+var map, infoWindow, denuncia, mDescripcion;
+var mLocation = null;
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Ubicación actual');
+            infoWindow.open(map);
+            map.setCenter(pos);
+            mLocation = pos;
+
+            // console.log(mLocation)
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+            mLocation = place.geometry.location;
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
+    // console.log(mLocation);
+}
 function saveDenuncia() {
-  var mDescripcion;
-  var mLocation;
-  var datetime = new Date().getTime();
-  var user = firebase.auth().currentUser.getUid();
-  var mAliasM;
-  /*Aca va el codigo con el que se obtiene la informacion
-  de los INPUTS con la instruccion
-   document.getElementById("ID").value;
-   Todo INPUT del HTML debe
-   tener un id (id="el_id")*/
+    var mDescripcion;
+    var mAliasM;
+    var user = firebase.auth().currentUser.getUid;
+    var datetime = new Date().getTime();
+    /*Aca va el codigo con el que se obtiene la informacion
+    de los INPUTS con la instruccion
+     document.getElementById("ID").value;
+     Todos los INPUT del HTML debe
+     tener un id (id="el_id")*/
+    mDescripcion = document.getElementById("txt_descripcion").toString();
+    denuncia = {descripcion:mDescripcion, idLugar:mLocation,
+        fechaHora:datetime, idUsuario:user,
+        alias:"mAlias"};
+    var select_location = document.getElementById("select_ubicacion");
+    var selection = select_location.options[select_location.selectedIndex].value;
 
+    if(selection === "0"){
+        alert(denuncia)
+    }
+    // firebase.database().ref("denuncias").push(denuncia);
+}
 
-  var denuncia = {descripcion:mDescripcion, idLugar:mLocation,
-                            fechaHora:datetime, idUsuario:user,
-                             alias:"mAlias"};
-  firebase.database().ref("denuncias").push(denuncia);
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
